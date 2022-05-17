dd9a6be580e849f8095aaea097b6d27772d5ec32
corvofeng <corvofeng@gmail.com>
2022-05-17 22:01:27 +0800

    Update the action.
    Signed-off-by: corvofeng <corvofeng@gmail.com>

---
+++ upterm.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+VERSION=v0.8.7
+
+case "$(uname -s)" in
+
+   Darwin)
+     echo 'Mac OS X'
+     wget -O /tmp/upterm https://github.com/corvofeng/upterm/releases/download/${VERSION}/upterm_darwin_amd64
+     ;;
+
+   Linux)
+     echo 'Linux'
+     wget -O /tmp/upterm https://github.com/corvofeng/upterm/releases/download/${VERSION}/upterm_linux_amd64
+     ;;
+
+   CYGWIN*|MINGW32*|MSYS*|MINGW*)
+     echo 'MS Windows'
+     echo "upterm doesn't support windows"
+     exit 0
+     ;;
+
+   # Add here more strings to compare
+   # See correspondence table at the bottom of this answer
+
+   *)
+     echo 'Other OS' 
+     ;;
+esac
+
+chmod +x /tmp/upterm
+/tmp/upterm host --vscode --verify-host-key=false bash
+
